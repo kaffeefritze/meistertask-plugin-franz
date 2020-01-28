5b82533717a9f7249825b6a05d1544e84839f434
lalamber <marcelpietsch@gmail.com>
2020-01-28 12:16:16 +0100

    Update index.js
    Added overrideUserAgent() to login with google auth.
    The standard browser seems to be unsecure for google now

--- meistertask/index.js
+++ meistertask/index.js
@@ -1,5 +1,6 @@
 'use strict';
-module.exports = Franz => Franz;
+//module.exports = Franz => Franz;
+module.exports = Franz => class Meistertask extends Franz { overrideUserAgent() { return "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136"; } };
 // module.exports = Franz => class Discord extends Franz {
 
 // };
